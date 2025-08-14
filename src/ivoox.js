7af3fc4ba8d53467ea05919a79c94edfb5dc2779
Kike Cabrera <kikecalpe@gmail.com>
2025-08-14 16:29:24 +0200

    user-agent change, https with keepAlive, and timeout increased

--- src/ivoox.js
+++ src/ivoox.js
@@ -47,7 +47,22 @@ async function getEpisodes(url, date, requestWait = 2000, next = false) {
   }
   const episodes = [];
 
-  const response = await axios.get(url);
+  const https = require('https');
+
+  const agent = new https.Agent({
+    keepAlive: true
+  });
+
+  const response = await axios.get(url, {
+    timeout: 20000,
+    httpsAgent: agent,
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+      'Accept-Language': 'es-ES,es;q=0.9,en;q=0.8'
+    }
+  });
+
   const dom = new jsdom.JSDOM(response.data);
   const pageEpisodes = parseIvoox(dom.window.document);
   const filteredEpisodes = pageEpisodes.filter(episode => episode.date > date);
